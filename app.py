dd8a72fc77710d8f694d48c653c4887d3c6832d4
Bibek Pradhan <134042662+ADD2LIST@users.noreply.github.com>
2023-06-25 08:43:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 import streamlit as st
-import webbrowser
+import requests
 
-def convert_to_view_source_url(url):
-    if url.startswith("http://"):
-        url = url[7:]
-    elif url.startswith("https://"):
-        url = url[8:]
-    
-    return f"view-source:{url}"
+def download_source_code(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.text
+        return content
+    else:
+        return None
 
 def main():
-    st.title("Webpage View Source Redirect")
-    st.write("Enter the URL of a webpage and click the button to view its source.")
+    st.title("Webpage Source Code Downloader")
+    st.write("Enter the URL of a webpage and click the button to download its source code.")
 
     url = st.text_input("Enter the URL")
-    view_source_url = convert_to_view_source_url(url)
 
-    if st.button("View Source"):
-        webbrowser.open_new_tab(view_source_url)
+    if st.button("Download Source Code"):
+        source_code = download_source_code(url)
+        if source_code is not None:
+            st.code(source_code, language="html")
+            st.download_button("Download", data=source_code, file_name="source_code.html", mime="text/html")
+        else:
+            st.write("Failed to download the source code.")
 
 if __name__ == "__main__":
     main()
-    
+
